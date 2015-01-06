108964ae75444747075ea1d426d64e354a42c8a2
Roman Ilyenko <ilienko.roman@gmail.com>
2015-01-06 03:03:17 +0300

    Add XML parser for a new version of BSUIR API

---
+++ ScheduleParser/SchemaToObjectTree.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ScheduleParser
+{
+    public class SchemaToObjectTree
+    {
+        public static TObject ParseDocument<TObject>(Stream rawStream)
+        {
+            TObject result = default(TObject);
+            XmlReader reader = XmlReader.Create(rawStream, new XmlReaderSettings(){IgnoreComments = true});
+            XmlSerializer serializer = new XmlSerializer(typeof(TObject));
+
+            if (serializer.CanDeserialize(reader))
+            {
+                result = (TObject)serializer.Deserialize(reader);
+            }            
+
+            return result;
+        }
+    }
+}
